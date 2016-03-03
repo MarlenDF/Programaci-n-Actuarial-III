2a0652c1993d38b0b204d9e605fe46ae94d999e5
Marlen DF <marlen.df@hotmail.com>
2016-03-03 16:20:57 -0600

    Práctica
    Esta es la práctica de If.

---
+++ Práctica.R
@@ -0,0 +1,19 @@
+y <- runif(1)
+y
+if(y<=30) {
+    msg <- "hola"
+    
+} else if(y>30 && y<=50){
+    msg <- "qué onda"
+    
+} else if(y>50 && y<=60){
+    msg <- "qué pedo wey"
+    
+} else if(y>60 && y<=80){
+    msg<- "qué pex"
+    
+} else {
+    msg <- "quiubo"
+    
+} 
+msg
